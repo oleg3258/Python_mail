59dfdaad6028a4a013ec805173f1b4bef2ec0cd3
Олег <swinkin55@yandex.ru>
2018-02-27 23:41:13 +0300

    написал чето

--- client.py
+++ client.py
@@ -9,7 +9,7 @@ class Client:
     #сохраняем метрику на сервере
 async def put(self, key, value):
     loop = asyncio.get_event_loop()
-    writer = await asyncio.open_connection(self.host, self.port, loop = loop)
+    writer = await asyncio.open_connection(self.host, self.port, loop = loop) хуй
 
     message =
     writer.write()
